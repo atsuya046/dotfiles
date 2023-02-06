3fdc732ab39d1abe5b4ea786980d29e94857b7b3
Nobuya  Oshiro <nobuya.oshiro@gmail.com>
2023-02-06 14:53:56 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,9 @@ if ! command -v brew > /dev/null 2>&1; then
     # Install homebrew: https://brew.sh/
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo
+    echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/s10129/.zprofile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/s10129/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 brew bundle
 echo
